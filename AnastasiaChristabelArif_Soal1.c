ff4ad8dbfaf0c059bdc65170e1819ee20fa566cd
crystalbel <71251225+crystalbel@users.noreply.github.com>
2021-06-16 14:49:10 +0700

    10th module

--- AnastasiaChristabelArif_Soal1.c
+++ AnastasiaChristabelArif_Soal1.c
@@ -1,45 +1,77 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main(){
-	int x = 0;
-	int y = 0;
-	int n_perintah;
-	int count = 0;
-	char perintah;
-	
-	printf("Masukkan banyak perintah:");
-	scanf("%d", &n_perintah);
-	
-	if(n_perintah >= 0){
-		while(count < n_perintah){
-			printf("\nMasukkan perintah:");
-			scanf(" %c", &perintah);
-			
-			if(perintah == 'U'){
-				y = y + 1;
-			}
-			else if(perintah == 'S'){
-				y = y - 1;
-			}
-			else if(perintah == 'T'){
-				x = x + 1;
-			}
-			else if(perintah == 'B'){
-				x = x - 1;
-			}
-			else if(perintah == 'H'){
-				break;
-			}
-			else{
-				printf("\nPosisi meong bros tetap");
-			}
-			
-			count = count + 1;
-		}
-		printf("\nKarakter Meong Bros berada di koordinat (x = %d, y = %d)", x, y);
-	}
-	else{
-		printf("\nBanyak perintah harus bilangan positif");
-	}
-	return 0;
+int i;
+
+struct rocket
+{
+    char name[50];
+    char size[10];
+    int mesin;
+    int booster;
+    char negara[50];
+} * rocket_ptr;
+
+void input(struct rocket *ptr);
+
+int main()
+{
+    rocket_ptr = (struct rocket *)malloc(3 * sizeof(struct rocket));
+    int total_cost = 0;
+    int cost[3] = {0};
+
+    input(rocket_ptr);
+
+    for (i = 0; i < 3; i++)
+    {
+        if (strcmp((rocket_ptr + i)->size, "Large") == 0)
+        {
+            cost[i] += 1000000;
+        }
+        else if (strcmp((rocket_ptr + i)->size, "Medium") == 0)
+        {
+            cost[i] += 500000;
+        }
+        else if (strcmp((rocket_ptr + i)->size, "Small") == 0)
+        {
+            cost[i] += 300000;
+        }
+    }
+
+    for (i = 0; i < 3; i++)
+    {
+        cost[i] += ((rocket_ptr + i)->mesin * 500000) + ((rocket_ptr + i)->booster * 700000);
+        if (strcmp((rocket_ptr + i)->negara, "USA") == 0)
+        {
+            cost[i] = cost[i] * 0.8;
+        }
+        total_cost += cost[i];
+    }
+
+    printf("\n=== HASIL HITUNGAN COST ===\n");
+    for (i = 0; i < 3; i++)
+    {
+        printf("%s ($ %d)\n", (rocket_ptr + i)->name, cost[i]);
+    }
+
+    printf("Total cost: %d", total_cost);
+    return 0;
+}
+
+void input(struct rocket *ptr)
+{
+    for (i = 0; i < 3; i++)
+    {
+        printf("\nNama roket %d: ", i + 1);
+        scanf("%s", (ptr + i)->name);
+        printf("Ukuran roket: ");
+        scanf("%s", (ptr + i)->size);
+        printf("Jumlah mesin roket: ");
+        scanf("%d", &(ptr + i)->mesin);
+        printf("Jumlah booster roket: ");
+        scanf("%d", &(ptr + i)->booster);
+        printf("Negara pemilik roket: ");
+        scanf("%s", (ptr + i)->negara);
+    }
 }
